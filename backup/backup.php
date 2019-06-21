ffae06adc574b3b00ce40fc9f2bfe3f9a047e590
ogonzalez29 <oscar.d.gonzalez@gmail.com>
2019-06-21 15:44:18 -0500

    changed backup script to work with php-cli for cron job

--- backup/backup.php
+++ backup/backup.php
@@ -10,18 +10,24 @@ $whitelist = [
     '127.0.0.1',
     '::1'
 ];
-if(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
-    $filename = $_SERVER["DOCUMENT_ROOT"].'../config.ini';
-    // echo "You are working in webserver";
+if(PHP_SAPI == 'cli'){
+    define('ROOT', dirname(__FILE__));
+    $filename = ROOT.'/../../../config.ini';
+    // echo "You are working in webserver an using php-cli";
 } 
-else {
+elseif(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
+    $filename = $_SERVER["DOCUMENT_ROOT"].'/../../../config.ini';
+    // echo "You are working in webserver and using cgi-fcgi";
+}
+else{
     $filename = $_SERVER["DOCUMENT_ROOT"].'/config.ini';
     // echo "You are working in localhost";
 }
 
 $data = parse_ini_file($filename, true);
+// print_r($data);
 
-// //Configure Dropbox Application
+//Configure Dropbox Application
 $app = new DropboxApp($data['dropbox_app_config']['client_id'],$data['dropbox_app_config']['client_secret'],$data['dropbox_app_config']['access_token']);
 
 // //Configure Dropbox service
@@ -32,21 +38,35 @@ $dbuser = $data['crm_db_config']['username'];
 $dbpass = $data['crm_db_config']['password'];
 $dbname = $data['crm_db_config']['dbname'];
 
-//Launch backup via command
-if(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)){    
-    exec("mysqldump -u $dbuser -p$dbpass $dbname db_backup.sql");
+// //Launch backup via php-cli or cgi-fcgi
+if(PHP_SAPI == 'cli' || !in_array($_SERVER['REMOTE_ADDR'], $whitelist)){    
+    exec('mysqldump -u '.$dbuser.' -p'.$dbpass.' '.$dbname.' | gzip > db_backup.sql.gz');
 }
 else {
     exec('C:\xampp\mysql\bin\mysqldump -u '.$dbuser.' -p'.$dbpass.' '.$dbname.' > db_backup.sql');
 }
 
 // //Prepare file for upload
-$dropboxFile = new DropboxFile(__DIR__ . "/db_backup.sql");
+if(PHP_SAPI == 'cli'){
+    $dropboxFile = new DropboxFile(ROOT . "/../../../db_backup.sql.gz");
+}
+elseif(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+    $dropboxFile = new DropboxFile(__DIR__ . "/db_backup.sql.gz");
+}
+else {
+    $dropboxFile = new DropboxFile(__DIR__ . "/db_backup.sql");
+}
 $dt = (new DateTime()) -> format("Y-m-d_H-i_s");
 
 try{
-    $file = $dropbox->upload($dropboxFile, "/backups/bcp".$dt.".sql", ['autorename' => true]);
-    echo $file->getName();
+    if(PHP_SAPI == 'cli' || !in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+        $file = $dropbox->upload($dropboxFile, "/backups/bcp".$dt.".sql.gz", ['autorename' => true]);
+        echo $file->getName();
+    }
+    else{
+        $file = $dropbox->upload($dropboxFile, "/backups/bcp".$dt.".sql", ['autorename' => true]);
+        echo $file->getName();
+    }
 }catch(Exception $e){
     echo $e;
 }
